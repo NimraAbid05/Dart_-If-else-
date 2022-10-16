7981c18b420006e7e8292f231af91c7727770729
NimraAbid05 <nimram214@gmail.com>
2022-10-16 01:27:12 -0700

    Check vowel or not

---
+++ vowel.dart
@@ -0,0 +1,17 @@
+void main() {
+  String n = "A";
+  if (n == 'a' ||
+      n == 'A' ||
+      n == 'e' ||
+      n == 'E' ||
+      n == 'i' ||
+      n == 'I' ||
+      n == 'O' ||
+      n == 'o' ||
+      n == 'u' ||
+      n == 'U') {
+    print("Vowel");
+  } else {
+    print("Consonant");
+  }
+}
